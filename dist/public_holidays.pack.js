eb598e490f797644c3226abf70e09c2d460517dd
pasierb <michal.pasierbski@sooftly.com>
2013-08-31 21:22:51 +0200

    fix austrian holidays

--- dist/public_holidays.pack.js
+++ dist/public_holidays.pack.js
@@ -230,44 +230,47 @@ PublicHolidays.registerFactory("pl", {
 
 PublicHolidays.registerFactory('at', {
   holidays: function(year) {
-    var context;
-
-    context = Date.today().set({
-      year: year
-    });
     return [
-      PublicHolidays.newYear(year), new PublicHolidays.Day(context.set({
+      PublicHolidays.newYear(year), new PublicHolidays.Day((Date.today()).set({
         day: 6,
-        month: 0
+        month: 0,
+        year: year
       }), "three_kings"), new PublicHolidays.Day(PublicHolidays.easter(year).date.add({
         days: 1
-      }), "easter_monday"), new PublicHolidays.Day(context.set({
+      }), "easter_monday"), new PublicHolidays.Day((Date.today()).set({
         month: 4,
-        day: 1
+        day: 1,
+        year: year
       }), "labour_day"), new PublicHolidays.Day(PublicHolidays.easter(year).date.add({
         days: 39
       }), "feast_of_the_ascension"), new PublicHolidays.Day(PublicHolidays.easter(year).date.add({
         days: 48
       }), "pentecost"), new PublicHolidays.Day(PublicHolidays.easter(year).date.add({
         days: 60
-      }), "corpus_christi"), new PublicHolidays.Day(context.set({
+      }), "corpus_christi"), new PublicHolidays.Day((Date.today()).set({
         month: 7,
-        day: 15
-      }), "maria"), new PublicHolidays.Day(context.set({
+        day: 15,
+        year: year
+      }), "maria"), new PublicHolidays.Day((Date.today()).set({
         month: 9,
-        day: 26
-      }), "austrian_national_day"), new PublicHolidays.Day(context.set({
+        day: 26,
+        year: year
+      }), "austrian_national_day"), new PublicHolidays.Day((Date.today()).set({
         month: 10,
-        day: 1
-      }), "all_saints"), new PublicHolidays.Day(context.set({
+        day: 1,
+        year: year
+      }), "all_saints"), new PublicHolidays.Day((Date.today()).set({
         month: 11,
-        day: 8
-      }), "immaculate_conception"), new PublicHolidays.Day(context.set({
+        day: 8,
+        year: year
+      }), "immaculate_conception"), new PublicHolidays.Day((Date.today()).set({
         month: 11,
-        day: 25
-      }), "christmas"), new PublicHolidays.Day(context.set({
+        day: 25,
+        year: year
+      }), "christmas"), new PublicHolidays.Day((Date.today()).set({
         month: 11,
-        day: 26
+        day: 26,
+        year: year
       }), "st_stephen")
     ];
   }
